c309682f30afee9b8b5c33f14bef5e9ce05d84aa
Joaquin <joaquin.melone.zarauza@gmail.com>
2022-12-06 22:07:50 -0300

    Update script.js and style.css

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
-const hugeContainer = document.querySelector(".container");
+// Variables.
 
-function createBox(number) {
-    for (let i = 0; i < number; i++) {
-        const smallerContainer = document.createElement("div");
-        smallerContainer.style.cssText = "display: flex; justify-content: center; align-items: center; width: 800px; height: 50px; border: 1px solid blue;";
-    
-        for (let j = 0; j < number; j++) {
-            const div = document.createElement("div");
-            div.classList.add("pixel");
-            div.style.cssText = "width: 50px; height: 50px; border: 1px solid blue;";
-            smallerContainer.appendChild(div);
-        }
-    
-        hugeContainer.appendChild(smallerContainer);
-    }
-}
+let numberOfPixels;
+let attempts = 0;
 
-const buttonContainer = document.querySelector(".prompt-button");
+// Button to choose the amount of pixels.
+
+const buttonContainer = document.querySelector(".button-box");
 
 const buttonPrompt = document.createElement("button");
 buttonPrompt.textContent = "Number of pixels";
-buttonPrompt.style.cssText = "width: 150px; height: 35px;"
+buttonPrompt.classList.add("prompt-button");
+buttonPrompt.style.cssText = "width: 150px; height: 35px;";
+
+buttonPrompt.addEventListener('click', () => {
+    numberOfPixels = prompt("Elige el número de pixeles que deseas ver en el cuadrado.");
+    createBox(numberOfPixels);
+    attempts += 1;
+  });
 
 buttonContainer.appendChild(buttonPrompt);
 
-createBox(5);
+// Container for the pixels.
+
+const hugeContainer = document.querySelector(".container");
+
+// This function creates the amount of pixels on the screen. And also removes the pixels that took place before.
+
+function createBox(number) {
+
+    // When the user already used the button to change the number of pixels, we remove the pixels that were before in order to create the new grid.
+     
+    if (attempts > 0) {
+        while (hugeContainer.hasChildNodes()) {
+            hugeContainer.removeChild(hugeContainer.firstChild);
+        }
+    }
+    
+    // This 2 for loops are used to create the columns and rows necessary to represent an Number x Number grid.
+
+    for (let i = 0; i < number; i++) {
+
+        const smallerContainer = document.createElement("div");
+        smallerContainer.setAttribute("id", "smallerContainer");
+        smallerContainer.style.cssText = "display: flex; border: 1px solid blue;";
+    
+            for (let j = 0; j < number; j++) {
+
+                const div = document.createElement("div");
+                div.setAttribute("id", "pixel");
+                smallerContainer.appendChild(div);
+
+            }
+
+        hugeContainer.appendChild(smallerContainer);
+    }
+
+    
+}
+
 
 
